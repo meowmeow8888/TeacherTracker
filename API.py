50b5211257544332dcecf53119c2d78fce28af1e
meowmeow8888 <mosseriguy8@gmail.com>
2024-12-20 20:09:09 +0200

    Add files via upload

--- API.py
+++ API.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from flask import Flask, jsonify
+from flask_cors import CORS
 from SchoolSchedule import SchoolSchedule
 from Teacher import Teacher
 
@@ -9,6 +10,7 @@ schedule = SchoolSchedule(day, "https://herzogks.iscool.co.il")
 schedule.fetch_data()
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000", "teachertracker.netlify.app"])
 
 @app.route("/teacher/<string:teacher_name>/<int:hour>",methods=["GET"])
 def get_room_for_teacher_and_hour(teacher_name:str, hour:int):
@@ -20,4 +22,4 @@ def get_empty_rooms_for_hour(hour:int):
 
 @app.route("/teachers",methods=["GET"])
 def get_teachers():
-    return jsonify({"teachers":schedule.teacher_map.keys()})
+    return jsonify({"teachers":list(schedule.teacher_map.keys())})
